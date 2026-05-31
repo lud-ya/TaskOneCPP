a254cb9a2e9a7e547102eba54152f4f7a0aa8998
Luidmyla <lud_ya@ukr.net>
2026-05-31 09:46:59 +0300

    Додав пустий проект С++

---
+++ Project54/Source.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(65001);
+	SetConsoleOutputCP(65001);
+	cout << "Привіт! Сьогодні чудовий день! :)"; 
+}
