2d9b94f95849142088627e96c0a426f01c30fc5a
Luidmyla <lud_ya@ukr.net>
2026-05-31 10:14:21 +0300

    Додав масив і ввів його з консолі

--- Project54/Source.cpp
+++ Project54/Source.cpp
@@ -7,4 +7,10 @@ int main()
 	SetConsoleCP(65001);
 	SetConsoleOutputCP(65001);
 	cout << "Привіт! Сьогодні чудовий день! :)"; 
+	char name[80];
+	cout << "Як Вас звати?\n";
+	cout << "->_";
+	cin.getline(name, 80, '\n');
+	cout << "Приємно познайомитися " << name << " :)\n";
+	return 0;
 }
